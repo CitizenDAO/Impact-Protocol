90cec3b3f05c34da0ae5e5f895e40f71d0e9920c
James Cunningham <j.a.cunningham@gmail.com>
2021-10-10 15:51:14 +0100

    Make interface functions external

--- packages/hardhat/contracts/IFundingPoolManager.sol
+++ packages/hardhat/contracts/IFundingPoolManager.sol
@@ -4,14 +4,14 @@ pragma solidity >=0.8.0 <0.9.0;
 
 interface IFundingPoolManager {
 
-    function purchase(uint256 poolId, address to, uint256 maturityDate) public payable returns (uint256);
+    function purchase(uint256 poolId, address to, uint256 maturityDate) external payable returns (uint256);
 
-    function redeem(uint256 poolId, uint256 bondId) public returns (bool);
+    function redeem(uint256 poolId, uint256 bondId) external returns (bool);
 
-    function transfer(uint256 poolId, uint256 bondId, address to) public returns (bool);
+    function transfer(uint256 poolId, uint256 bondId, address to) external returns (bool);
 
-    function getPoolInfo(uint256 poolId) public view returns (address asset, uint256 apy, uint256 outstanding);
+    function getPoolInfo(uint256 poolId) external view returns (address asset, uint256 apy, uint256 outstanding);
 
-    function getBondInfo(uint256 poolId, uint256 bondId) public view returns (address, uint256, address, uint256, bool);
+    function getBondInfo(uint256 poolId, uint256 bondId) external view returns (address, uint256, address, uint256, bool);
 
 }
