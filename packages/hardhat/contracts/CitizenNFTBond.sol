c2bdec281730d3bd115e1f5d4811876ca146f1eb
James Cunningham <j.a.cunningham@gmail.com>
2021-10-10 15:12:59 +0100

    Fix ERC721Enumerable reference

--- packages/hardhat/contracts/CitizenNFTBond.sol
+++ packages/hardhat/contracts/CitizenNFTBond.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "./IFundingPoolManager.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 
