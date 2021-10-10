def53704de2591276ad844c722c9812d72297413
James Cunningham <j.a.cunningham@gmail.com>
2021-10-10 16:15:17 +0100

    Add stubbed implementation for FundingPoolManager

--- packages/hardhat/contracts/FundingPoolManager.sol
+++ packages/hardhat/contracts/FundingPoolManager.sol
@@ -4,14 +4,24 @@ pragma solidity >=0.8.0 <0.9.0;
 
 contract FundingPoolManager is IFundingPoolManager {
     
-    function purchase(uint256 poolId, address to, uint256 maturityDate) public payable returns (uint256);
+    function purchase(uint256 poolId, address to, uint256 maturityDate) external virtual payable returns (uint256) {
+        return 0;
+    }
 
-    function redeem(uint256 poolId, uint256 bondId) public returns (bool);
+    function redeem(uint256 poolId, uint256 bondId) external virtual returns (bool) {
+        return true;
+    }
 
-    function transfer(uint256 poolId, uint256 bondId, address to) public returns (bool);
+    function transfer(uint256 poolId, uint256 bondId, address to) external virtual returns (bool) {
+        return true;
+    }
 
-    function getPoolInfo(uint256 poolId) public view returns (address asset, uint256 apy, uint256 outstanding);
+    function getPoolInfo(uint256 poolId) external virtual view returns (address asset, uint256 apy, uint256 outstanding) {
+        return (address(0x0), 0, 0);
+    }
 
-    function getBondInfo(uint256 poolId, uint256 bondId) public view returns (address, uint256, address, uint256, bool);
+    function getBondInfo(uint256 poolId, uint256 bondId) external virtual view returns (address, uint256, address, uint256, bool) {
+        return (address(0x0), 0, address(0x0), 0, true);
+    }
 
 }
