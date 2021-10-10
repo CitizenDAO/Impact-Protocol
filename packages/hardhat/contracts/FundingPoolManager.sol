dc85b9c84cd795a7e37c3ffee422ad08a763ba01
james-c <j.a.cunningham@gmail.com>
2021-10-10 16:24:55 +0100

    Declare override correctly

--- packages/hardhat/contracts/FundingPoolManager.sol
+++ packages/hardhat/contracts/FundingPoolManager.sol
@@ -6,23 +6,23 @@ import "./IFundingPoolManager.sol";
 
 contract FundingPoolManager is IFundingPoolManager {
     
-    function purchase(uint256 poolId, address to, uint256 maturityDate) external virtual payable returns (uint256) {
+    function purchase(uint256 poolId, address to, uint256 maturityDate) external override payable returns (uint256) {
         return 0;
     }
 
-    function redeem(uint256 poolId, uint256 bondId) external virtual returns (bool) {
+    function redeem(uint256 poolId, uint256 bondId) external override returns (bool) {
         return true;
     }
 
-    function transfer(uint256 poolId, uint256 bondId, address to) external virtual returns (bool) {
+    function transfer(uint256 poolId, uint256 bondId, address to) external override returns (bool) {
         return true;
     }
 
-    function getPoolInfo(uint256 poolId) external virtual view returns (address asset, uint256 apy, uint256 outstanding) {
+    function getPoolInfo(uint256 poolId) external override view returns (address asset, uint256 apy, uint256 outstanding) {
         return (address(0x0), 0, 0);
     }
 
-    function getBondInfo(uint256 poolId, uint256 bondId) external virtual view returns (address, uint256, address, uint256, bool) {
+    function getBondInfo(uint256 poolId, uint256 bondId) external override view returns (address, uint256, address, uint256, bool) {
         return (address(0x0), 0, address(0x0), 0, true);
     }
 
